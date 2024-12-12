b0f9360a0c3cbce9eccee114ae4b47913035f74e
Pablo  Aguilar <135460549+pablo-972@users.noreply.github.com>
2024-12-12 17:59:38 +0100

    Update LeafletControlGeocoder.jsx

--- src/maps/LeafletControlGeocoder.jsx
+++ src/maps/LeafletControlGeocoder.jsx
@@ -56,7 +56,7 @@ function LeafletControlGeocoder({setCoordinates = null}) {
 
       const latlng = e.geocode.center;
       const icon = L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png', 
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png', 
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
